7000e54b942987a4c915715df105880e8933f477
username <1037370201@qq.com>
2018-07-22 17:45:42 +0800

    调用方法

--- src/First.java
+++ src/First.java
@@ -1,5 +1,9 @@
 public class First {
     public static void main(String[] args){
         System.out.print("hello word");
+        show();
+    }
+    private static void show(){
+        System.out.println("aabbcc");
     }
 }
